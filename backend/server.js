945fd92f4954e05581d179f38a32a06d6c47707f
Christopher Wargo <crwargo0783@gmail.com>
2022-09-25 19:57:37 -0400

    connected db and initial controller work

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const colors = require('colors');
+const dotenv = require('dotenv').config();
+const {errorHandler} = require('./middleware/errorMiddleWare')
+const connectDB = require('./config/db');
+const port = process.env.PORT || 5000;
+
+connectDB()
+const app = express();
+
+// Accept body data
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+app.use('/api/goals', require('./routes/goalRoutes'))
+
+app.use(errorHandler)
+app.listen(port, () => console.log(`Server started on port ${port}`))
