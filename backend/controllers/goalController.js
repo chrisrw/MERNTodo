56819a86ed57b630b0aafbe922f1a79da4591720
Christopher Wargo <crwargo0783@gmail.com>
2022-09-28 12:26:21 -0400

    finished CRUD backend

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -48,14 +48,15 @@ const updateGoal = asyncHandler(async(req, res) => {
 // @route   DELETE /api/goals/:id
 // @access  Private
 const deleteGoal = asyncHandler(async(req, res) => {
+    const goal = await Goal.findById(req.params.id)
 
     if(!goal){
         res.status(400)
         throw new Error('Goal not found')
     }
 
-    const deletedGoal = await Goal.findByIdAndRemove(req.params.id)
-    res.status(200).json(deletedgoal);
+    await goal.remove()
+    res.status(200).json({id: req.params.id});
 })
 
 
